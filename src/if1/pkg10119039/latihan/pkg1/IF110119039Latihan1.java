39efe848fbf2086795be9eaccf23f7d9668c5e7b
diky <diky@diky-PC>
2020-10-17 13:23:28 +0700

    Upload Project

---
+++ src/if1/pkg10119039/latihan/pkg1/IF110119039Latihan1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119039.latihan.pkg1;
+
+/**
+ *
+ * @author 
+ * NAMA  : Diva Sabila Ramadhan
+ * KELAS : IF 1
+ * NIM   : 10119039
+ * Deskripsi Program : Program ini untuk menampilkan value ke layar 
+ */
+public class IF110119039Latihan1 {
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR");
+        System.out.println("========================================");
+        System.out.println("NAMA\t\t: Diva Sabila Ramadhan");
+        System.out.println("ALAMAT\t\t: Komp Permata Biru No 47");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 22 Desember 2000");
+        System.out.println("========================================");
+        System.out.println("HOBI\t\t: Membaca Artikel Otomotif");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Teh");
+    }
+    
+}
